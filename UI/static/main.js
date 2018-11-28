e5db7446626f8159da285c18bf3217bd5dde14af
zydplatform <kigandaivan32@gmail.com>
2018-11-28 07:29:42 -0800

    ui-design-using-css-js

---
+++ UI/static/main.js
@@ -0,0 +1,54 @@
+var login = document.getElementById('login');
+var wrongUsername = document.getElementById('wrong-name');
+var wrongPassword = document.getElementById('wrong-password');
+
+login.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    if (e.target.name.value == 'admin' && e.target.password.value == 'admin'){
+        e.target.name.value = '';        
+        window.location.href = '../IREPORT/UI/templates/allreports.html'
+    } else if (e.target.name.value == 'user' && e.target.password.value == 'user'){
+        e.target.name.value = '';
+        window.location.href = '../IREPORT/UI/templates/userreports.html';
+    }
+
+    if (e.target.name.value == '' || e.target.password.value == ''){
+        wrongUsername.style.display = 'block';
+        wrongUsername.innerText = 'Provide Username and Password';
+    } else if (e.target.name.value != 'admin' || e.target.password.value != 'admin'){
+        wrongUsername.style.display = 'block';
+        wrongUsername.innerText = 'wrong username or password';
+    }
+
+    if (e.target.name.value == '' || e.target.password.value == ''){
+        wrongPassword.style.display = 'block';
+        wrongPassword.innerText = 'Provide Username and Password';
+    } else if (e.target.name.value != 'user' || e.target.password.value != 'user'){
+        wrongPassword.style.display = 'block';
+        wrongPassword.innerText = 'wrong username or password';
+    }
+
+   
+});
+
+
+var a = document.getElementById("locate");
+
+function Location() {
+    if(navigator.geolocation){
+        navigator.geolocation.watchPosition(showreportPosition);
+    }
+    else {
+        a.innerText = "geolocation not supported by browser";
+    }
+}
+
+function showreportPosition(position) {
+    a.innerText="Lat:" + position.coords.latitude + "Long:" + position.coords.longitude;
+    alert("geolocation is set");
+}
+
+function Remove(){
+    alert("report deleted");
+}
---
+++ UI/static/signup.js
@@ -0,0 +1,17 @@
+var signup = document.getElementById('signup');
+
+signup.addEventListener('submit',function(p){
+    p.preventDefault();
+    if (p.target.fname.value != '' && p.target.password.value != ''&& p.target.lname.value && p.target.username.value && p.target.dispatchEvent.value
+        && p.target.email.value !=''&& p.target.phone.value !=''&& p.target.confirm.value !=''){
+        p.target.fname.value = '';
+        p.target.lname.value = '';
+        p.target.username.value = '';
+        p.target.date.value = '';
+        p.target.email.value = '';
+        p.target.phone.value = '';
+        p.target.password.value = '';
+        p.target.confirm.value = '';        
+        window.location.href = '../../index.html';
+}
+});
